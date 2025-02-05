1d811480080cb8bb6a10fb4198e5f079c1154500
yahay <124700074+Asimiraq@users.noreply.github.com>
2025-02-05 18:59:02 +0300

    2m.js

---
+++ 2m.js
@@ -0,0 +1,45 @@
+
+
+
+
+$(` <center><div></div><center></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$('a.label.label-primary.fl > img.fl').replaceWith('<img src="sico/z1casj3f6210.png" class="fl" style="margin-Right: 2px; width: 30px;">'),$(` <center><marquee direction="right" width="99%" id="asim55" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >قوانين الشات ● • في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● • وشكرآ لكم</marquee></font><center>
+`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$(`<div class="uzr fl corner borderg mm bid1cb1etoid10" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(&quot;pic/1cb1bqui110.jpg.jpg&quot;);" class="fl fitimg hand u-pic    ">
+      <span style="margin-top:2px;padding:0px 2px;margin-left:-20px;margin-right:4px;color:grey" class="fr minix tago" ago="1523688860481">2د</span>
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="sico/z1c9h490at10.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(77, 75, 69); background-color: rgb(209, 204, 194);" class="corner nosel u-topic dots fl hand">　　سآ’إْرٍة　　</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(0, 0, 0);" class=" u-msg   break  fl">
+أرَاكَ عليَّ أقْسَى النَّاسِ قَلباً
+ولي حالٌ ترقُّ لهُ القلوبُ
+حَبيبٌ أنتَ قلْ لي أمْ عَدوٌّ
+ففعلكَ ليسَ يفعلهُ حبيبُ !&nbsp;</div>
+      </div>
+&nbsp;<br />
+&nbsp;</div>
+      </div>
+    <a  style="margin-top:-20px;padding:4px;float:right;border-radius:100px;" class="btn minix btn-danger fa fa-spin fa-heart fr"></a></div>`).insertBefore('div#d2bc'),$(`<img src ="sico/z1c7j29n8e10.gif" etclass="fr" style="width:100%;margin-top: 1px;">
+</div>
+</style>`).insertBefore('body'),$(`
+
+<script type="text/javascript" src="https://iqr30.github.io/my-css-js-html/\u0634\u0627\u062A \u0639\u0631\u0627\u0642\u0646\u0627\u0627\u0627.js" />
+
+$('.fl.ustat')
+    .css('width', '3px'), $('.fa.fa-user-plus')
+    .html(`❥تسجيل `), document.getElementById('u1')
+    .placeholder = 'اكتب اسمك من 5 احرف فقط ', 
+document.getElementById('pass1')
+  .placeholder = 'كلمة المرور', document.getElementById('u2')
+    .placeholder = 'فخأمة طلتك تنورنا', document.getElementById('pass2')
+    .placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+    .placeholder = '♥ أكتب اسمك ولازم تحفظه ';
+
+ $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` <a class="fl mini fa fa-bullhorn  btn btnn-default">&nbsp; لـ الشكاوي .والاقتراحات . </a><input type="jfa-w" name="FirstName" value="( @jfa_w ) اتصال بناء: عبر تليجرام"><center>`).appendTo("#settings"),clearInterval(b)):console.log(c)},1)}); 
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+alert( "اهلا وسهلا بكم في شات جفى للجوال ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تـسجيـل )  (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+});
+$(`<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="jfa99">♥ || جـــفــى لـلـجــوال || ♥</font> ● · ‫أفضل سوبره لهذا الأسبوع · ● <font class="jfa99" color="#727A54">&nbsp;قريباً ♥ &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="jfa88" color="#ff9900">&nbsp;قريباً &nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="jfa88" color="#ff00ff">&nbsp; قريباً .. ♥  &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="jfa88" color="#5CAD9A">&nbsp; قريباً  &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="jfa88" color="#800000">&nbsp; قريباً &nbsp;</font> ● · ‫أفضل أدارية لهذا الأسبوع · ● <font class="jfa88" color="#417570">&nbsp; قريباً .. ♥  &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp; 
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
