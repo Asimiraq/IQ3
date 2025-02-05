1837b89cc0c2c0c31a46c8cba832a1895f53b6ca
yahay <124700074+Asimiraq@users.noreply.github.com>
2025-02-05 19:22:16 +0300

    n.js

--- n.js
+++ n.js
@@ -1,11 +1,11 @@
 $(` <center><div></div><center></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$('a.label.label-primary.fl > img.fl').replaceWith('<img src="sico/z1casj3f6210.png" class="fl" style="margin-Right: 2px; width: 30px;">'),$(` <center><marquee direction="right" width="99%" id="asim55" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >قوانين الشات ● • في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● • وشكرآ لكم</marquee></font><center>
 `).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$(`<div class="uzr fl corner borderg mm bid1cb1etoid10" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
-      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(&quot;pic/1cb1bqui110.jpg.jpg&quot;);" class="fl fitimg hand u-pic    ">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://fievhost.online/pic/1738666436541.png.webp);" class="fl fitimg hand u-pic    ">
       <span style="margin-top:2px;padding:0px 2px;margin-left:-20px;margin-right:4px;color:grey" class="fr minix tago" ago="1523688860481">2د</span>
       <div class="uzr fl" style="padding:0px;width:80%">
         <div style="padding:0px;width:100%;" class="fl">
           <img class="fl u-ico" alt="" src="sico/z1c9h490at10.gif">
-          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(77, 75, 69); background-color: rgb(209, 204, 194);" class="corner nosel u-topic dots fl hand">　　سآ’إْرٍة　　</span>
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(77, 75, 69); background-color: rgb(209, 204, 194);" class="corner nosel u-topic dots fl hand">　　الـ ناي　　</span>
         </div>
         <br>
         <div style="padding: 0px; width: 100%; color: rgb(0, 0, 0);" class=" u-msg   break  fl">
