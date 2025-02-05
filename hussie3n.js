abdd93c3a975ce171f54dd075e6f3c42dd6edc14
yahay <124700074+Asimiraq@users.noreply.github.com>
2025-02-05 21:37:54 +0300

    hussie3n.js

---
+++ hussie3n.js
@@ -0,0 +1,14 @@
+$.getScript("https://raw.githack.com/Asimiraq/IQ3/main/a.js");
+$(`<link rel="stylesheet" href="https://raw.githack.com/Asimiraq/IQ3/main/t5.css"></style>`).insertBefore("body")
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -3px; padding:6px; width:35px;margin-top: -2px;" src="https://k.top4top.io/p_17044g8ns1.gif" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){alert(" هذه الستايل تم تصميمه من قبل حسين النعيمي غير قابل للسرقه  ");});$('div#l2 button.btn.btn-primary')['bind']
+$('.fl.ustat')
+    .css('width', '3px'), $('.fa.fa-user-plus')
+    .html(`❥تسجيل `), document.getElementById('u1')
+    .placeholder = 'اكتب اسمك من 5 احرف فقط ', 
+document.getElementById('pass1')
+  .placeholder = 'كلمة المرور', document.getElementById('u2')
+    .placeholder = 'فخأمة طلتك تنورنا', document.getElementById('pass2')
+    .placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+    .placeholder = '♥ أكتب اسمك ولازم تحفظه ';
+$('.fa.fa-user-plus').html(`تسجيل`);(document.getElementById("tbox").placeholder = "عمي عوف السطح ودردش ويانا");
