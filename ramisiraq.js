a5ad7b2f7bdbca503b788cd8d58578e4bbffc59e
yahay <124700074+Asimiraq@users.noreply.github.com>
2025-02-07 21:03:36 +0300

    ramisiraq.js

---
+++ ramisiraq.js
@@ -0,0 +1,285 @@
+$(`<style>
+@font-face{font-family:'jazeera';src: url('https://www.fontstatic.com/fonts/jazeera/jazeera.eot?#iefix');src: local('الجزيرة'), local('jazeera'),url('https://www.fontstatic.com/fonts/jazeera/jazeera.woff') format('woff');}
+@font-face{font-family:'jazeera-light';src: url('https://www.fontstatic.com/fonts/jazeera-light/jazeera-light.eot?#iefix');src: local('الجزيرة خفيف'), local('stc'),url('https://www.fontstatic.com/fonts/stc/stc.ttf') format('woff');}
+
+.nav-tabs {
+    margin-top: 2px !important;
+    float: right;
+    border-top: 3px solid;
+    height: 30px !important;
+    color: #ffffff;
+}
+ul.nav.nav-tabs.fl li {
+    /* width: 33.3%; */
+    margin-left: 0;
+    border-radius: 50px;
+    margin-bottom: 5px !important;
+}
+
+ul.nav.nav-tabs.fl li a {
+text-align: center;
+    padding: 0px 0 !important;
+    border: 0 solid #4c4b4b;
+    border-radius: 25px;
+    olor: #fefffc;
+    border: none !important;
+    margin: 0;
+    height: 26px;
+    text-align: center;
+    border-radius: 6px;
+    margin-bottom: 2px;
+    padding: 5px 0 !important;
+    width: 123px !important;
+    margin-left: 5px !important;
+    background: #000 !important;
+font-family: "stc";
+
+}
+div.tab-content.fl {
+padding: .4rem .4rem 0;
+}
+.nav-tabs {
+    border-bottom: 1px solid #ffffff;
+}
+div.tab-content.fl {
+opacity: 1 !important;
+    transition: none !important;
+    width: auto !important;
+    height: 100px !important;
+    margin: 0px !important;
+    position: relative;
+    top: 0 !important;
+    border-top: 0px solid !important;
+    margin-top: 5px !important;
+    border-radius: 15px !important;
+    border-bottom: 1px solid #dee2e600 !important;
+    border-left: 4px solid #f7d322 !important;
+    border-right: 4px solid #f7d322 !important;
+
+}
+ul.nav.nav-tabs>li>a {
+    color: #fff;
+}
+.nav-tabs>li.active>a, .nav-tabs>li.active>a:hover, .nav-tabs>li.active>a:focus {
+    color: #f7d322;
+    cursor: default;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-bottom-color: transparent;
+}
+
+div#l2 input#pass1, div#l2 input#u2, div#l3 input#pass2, div#l3 input#u3 {
+    padding: 0px 0px !important;
+    border-radius: 25px !important;
+    border: 1px solid #e1e0e0;
+    text-align: center;
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 1px 2px rgb(107 32 49);
+    border: 1px solid white;
+    height: 27px !important;
+    border: 1px solid #dedede !important;
+    text-align: center;
+    width: 49% !important;
+    padding: 0 !important;
+    float: right;
+    margin: 2px 3px 2px 0;
+    width: 60%;
+    padding: 0 !important;
+    float: right;
+    height: 25px;
+    margin: 4px 7px 5px 4px !important;
+    border-radius: 15px;
+    border: 1px solid #e1e0e0 !important;
+    text-align: center;
+    border-right: 7px solid !important;
+    border-left: 7px solid !important;
+    border-color: #000 !important;
+}
+div#l2 button {
+    border-radius: 24px !important;
+    height: 27px;
+    padding: 0px 5px;
+    width: 98%;
+    margin-left: 4px;
+    box-shadow: inset 0 0px 0 rgb(255 255 255), 0 2px 0px rgb(252 199 209);
+    border-color: #FFFFFF !important;
+    margin-top: 4px !important;
+margin-bottom: 6px;
+    margin-top: 2px;
+    border-radius: 15px 6px 6px 6px !important;
+    font-family: el messiri, FontAwesome !important;
+    width: 30%;
+    height: 30px;
+    margin-left: 4.5px !important;
+    border: 2px solid #e6e6fa !important
+}
+div#l1 input#u1, div#l3 button, div#l1 button {
+    border-radius: 24px !important;
+    height: 27px;
+    padding: 0px 5px;
+    width: 98%;
+    margin-left: 4px;
+    box-shadow: inset 0 0px 0 rgb(255 255 255), 0 2px 0px rgb(252 199 209);
+    border-color: #FFFFFF !important;
+    margin-top: 4px !important;
+margin-bottom: 6px;
+    margin-top: 2px;
+    border-radius: 15px 6px 6px 6px !important;
+    font-family: el messiri, FontAwesome !important;
+    width: 30%;
+    height: 30px;
+    margin-left: 4.5px !important;
+    border: 2px solid #e6e6fa !important
+}
+label.fl.nosel.label.pnhead {
+    background-color: #c11616;
+    text-align: center;
+    color: #fff !important;
+    word-wrap: break-word;
+    padding: 4px;
+    margin: 0 0 20px;
+    right: 99px !important;
+    border-radius: 59px 0px 68px 0px !important;
+    width: 130px;
+    background: linear-gradient(-45deg, #cc31cc, pink, #4997d0, #4caf50);
+    background-size: 400% 400%;
+font-family: "stc";
+
+}
+.fl.fa.fa-sign-in.btn.btn-primary.dots.roomh.border.corner {
+    background-color: #a88e34 !important;
+    padding: 1px 4px !important;
+    pointer-events: none;
+}
+#settings .tc {
+    border-radius: 3px !important;
+    padding: 7px !important;
+    border: 1px solid #2b2b2b !important;
+    margin: 6px 0 0 1px !important;
+    width: 99.6% !important;
+    margin-top: 2px !important;
+    cursor: pointer;
+    text-align: center;
+    padding-top: .3em;
+    border: 1px solid #000;
+    margin-top: 3px;
+    height: 32px;
+    line-height: 25px;
+    font-weight: 700;
+    position: relative;
+    margin-bottom: .2rem;
+    border-radius: 0 45px 37px 40px !important;
+    border: 1px solid #FFE0F6 !important;
+    font-family: 'jazeera', FontAwesome;
+
+}
+.tbox {
+    padding: 3px;
+    min-height: 28px !important;
+    border-right: 5px solid;
+    border-right-color: currentcolor;
+    border-right-style: solid;
+    border-left: 5px solid;
+    border-left-color: currentcolor;
+    border-left-style: solid;
+    border-color: #d2d2d2;
+    border-left-style: groove;
+    border-right-style: ridge;
+    border-radius: 3px !important;
+}
+#asim55 {
+    border-top: 2px solid #f7d322;
+    border-bottom: 2px solid #f7d322;
+    margin-bottom: 2px;
+    border-radius: 5px 5px 5px 5px;
+    font-family: 'jazeera', FontAwesome;
+    color: #000;
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before, ul.nav.nav-tabs>li>.fa-user:before, ul.nav.nav-tabs>li>.fa-user:before {
+font-family: "stc";
+display: none;
+}
+a.label.bg.d-flex.fl {
+display: none;
+}
+body {
+    background-image: url(https://g.top4top.io/p_1788q5rth1.gif);
+    background-size: contain;
+    background-color: #f9f9f9 !important;
+}
+.lonline.light.break {
+    outline: #ffffff00 solid 1px !important;
+    background-color: #5eb5bf3b !important;
+    background-image: -webkit-linear-gradient(45deg, rgba(255, 255, 255, .2) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, .2) 50%, rgba(255, 255, 255, .2) 75%, transparent 75%, transparent) !important;
+}
+div#tlogins ::-webkit-scrollbar {
+    width: 5px;
+    height: 10px;
+}
+
+div#tlogins ::-webkit-scrollbar-button {
+    width: 5px;
+    height: 5px;
+}
+div#tlogins ::-webkit-scrollbar-corner {
+    background: transparent;
+}
+div#tlogins ::-webkit-scrollbar-thumb {
+    background: #287c86;
+    border: 71px none #fff;
+    border-radius: 31px;
+    background-image: -webkit-linear-gradient(45deg, rgba(255, 255, 255, .2) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, .2) 50%, rgba(255, 255, 255, .2) 75%, transparent 75%, transparent) !important;
+}
+div#tlogins ::-webkit-scrollbar-track {
+    background: #fff;
+    border: 65px none #fff;
+    border-radius: 34px;
+}
+</style>`).insertBefore('body');
+$(`<link rel="stylesheet" type="text/css" href="https://www.fontstatic.com/f=stc" />
+`).insertBefore("body")
+$(`<a id="hussien" style="    width: 395px;
+    background: url(https://k.top4top.io/p_3325lz5cc1.gif) no-repeat center center;
+    background-size: 100% 100%;
+    /* margin: 2px; */
+    transform: matrix(1, 0, 0, 1, -4, 0);
+    overflow: hidden" class="label label label-primary mini fl" target="_blank" href="soon">
+    <marquee behavior='scroll' direction='right' scrollamount='3' style='width:100%; display: inline-block;'>اهلا وسهلا بكم في شات ريماس العراق</marquee></a></a>`).insertBefore('.nav-tabs');
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+alert( "اهلا وسهلا بكم في شات ريماس العراق للجوال ❤️ سجل عضوية تحصل على 5000 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تـسجيـل )  (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+});
+$(` <center><div></div><center></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$('a.label.label-primary.fl > img.fl').replaceWith('<img src="sico/z1casj3f6210.png" class="fl" style="margin-Right: 2px; width: 30px;">'),$(` <center><marquee direction="right" width="99%" id="asim55" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >قوانين الشات ● • في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● • وشكرآ لكم</marquee></font><center>
+`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary'),$(`<div class="uzr fl corner borderg mm bid1cb1etoid10" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://fievhost.online/pic/1738666436541.png.webp);" class="fl fitimg hand u-pic    ">
+      <span style="margin-top:2px;padding:0px 2px;margin-left:-20px;margin-right:4px;color:grey" class="fr minix tago" ago="1523688860481">2د</span>
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="sico/z1c9h490at10.gif">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(77, 75, 69); background-color: rgb(209, 204, 194);" class="corner nosel u-topic dots fl hand">　　الـ ناي　　</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(0, 0, 0);" class=" u-msg   break  fl">
+أرَاكَ عليَّ أقْسَى النَّاسِ قَلباً
+ولي حالٌ ترقُّ لهُ القلوبُ
+حَبيبٌ أنتَ قلْ لي أمْ عَدوٌّ
+ففعلكَ ليسَ يفعلهُ حبيبُ !&nbsp;</div>
+      </div>
+&nbsp;<br />
+&nbsp;</div>
+      </div>
+    <a  style="margin-top:-20px;padding:4px;float:right;border-radius:100px;" class="btn minix btn-danger fa fa-spin fa-heart fr"></a></div>`).insertBefore('div#d2bc'),$(``).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="sico/z1c68amgdt10.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$(`
+<link rel="stylesheet" href=""><style>
+
+
+</style>`).insertBefore('body')
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -3px; padding:6px; width:35px;margin-top: -2px;" src="https://k.top4top.io/p_17044g8ns1.gif" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){alert(" هذه الستايل تم تصميمه من قبل حسين النعيمي غير قابل للسرقه  ");});$('div#l2 button.btn.btn-primary')['bind']
+$('.fl.ustat')
+    .css('width', '3px'), $('.fa.fa-user-plus')
+    .html(`❥تسجيل `), document.getElementById('u1')
+    .placeholder = 'اكتب اسمك من 5 احرف فقط ', 
+document.getElementById('pass1')
+  .placeholder = 'كلمة المرور', document.getElementById('u2')
+    .placeholder = 'فخأمة طلتك تنورنا', document.getElementById('pass2')
+    .placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+    .placeholder = '♥ أكتب اسمك ولازم تحفظه ';
+$('.fa.fa-user-plus').html(`تسجيل`);(document.getElementById("tbox").placeholder = "عمي عوف السطح ودردش ويانا");
